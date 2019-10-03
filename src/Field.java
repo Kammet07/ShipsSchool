12692795110d62e56b68fc983616d023212272a8
Kammet07 <pavel.pupentsov@educanet.cz>
2019-10-03 23:04:32 +0200

    test

---
+++ src/Field.java
@@ -0,0 +1,13 @@
+public class Field {
+    public char[][] getFieldArr() {
+        char[][] field = new char[10][10];
+
+        for (int i = 0; i != 10; i++){
+            for (int y = 0; y != 10; y++){
+                field[i][y] = '-';
+            }
+        }
+
+        return field;
+    }
+}
